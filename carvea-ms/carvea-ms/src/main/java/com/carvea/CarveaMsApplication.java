890adee57a24630ce8beaa53929a79b012228ed0
Ardit Mirena <mirenaardit@gmail.com>
2024-11-05 12:59:39 +0100

    Created project setup files and added necessary dependecies

---
+++ carvea-ms/carvea-ms/src/main/java/com/carvea/CarveaMsApplication.java
@@ -0,0 +1,15 @@
+package com.carvea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarveaMsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CarveaMsApplication.class, args);
+		System.out.println("Elsa queen <3");
+	}
+
+}
---
+++ carvea-ms/carvea-ms/src/test/java/com/carvea/CarveaMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carvea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarveaMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
